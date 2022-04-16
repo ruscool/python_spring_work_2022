d516cfbe4c60db72d756749a6398be28db6ff31d
ruslan <ruscool79@yandex.ru>
2022-04-16 12:45:38 +0300

    предварительно работает

--- python_spring_work_2022/lesson__4__13.04.2022/hometask/task15.py
+++ python_spring_work_2022/lesson__4__13.04.2022/hometask/task15.py
@@ -33,57 +33,64 @@ words = ["оператор", "конструкция", "объект", "инфо
 desc_ = ["Это слово обозначает наименьшую автономную часть языка программирования",
          ".2.", "3 Это слово обозначает", "4 Это слово обозначает",
          "5 Это слово обозначает", "6 Это слово обозначает", "7 Это слово обозначает", "8 Это слово обозначает"]
-word_random = random.choice(words)
+word_random = str(random.choice(words))
+
 print(words, word_random)  # удалить потом
 print(desc_[words.index(word_random)])
 word_kod = "#" * len(word_random)
-print(word_kod, type(word_kod))
-# print(("# " * len(word_random)))
+print('Загаданное слово: ',' '.join(word_kod), type(word_kod))
 word_list = list(word_kod)
 word_ord = [ord(x) for x in word_random]
 print("ord", word_ord)
-# print(b)
+bufer = []
 
 while a > 0:
-    letter = input("Введите букву: ")
-    print(ord(letter))
-    count_list = []
-    b = 0
-    count_letter = word_random.count(letter)
+    letter = str(input("Введите букву: "))
+    bufer.sort()
+    ord_letter = ord(letter)
 
-    if count_letter > 1:
-        # count_list = []
-        for i in word_random:
-            if i == letter:
-                count_list.append(word_random.index(letter, b))
-                b = count_list[-1] + 1
+    print("ord_letter", ord_letter)
+    print("ord", word_ord)
+    print("bufer", bufer)
 
-        print("индексы", count_list, type(count_list))
-
-        if letter in word_random:
-            print("Есть такая буква")
-            for f in count_list:
-                print(word_kod, f)
-                if f > 0:
-                    # pass
-                    word_list[f] = word_random[f]
-                elif f == 0:
-                    # pass
-                    word_list[f] = word_random[f]
-                # d = word_list
-
-                # word_kod.replace(count_list[],)
-            print("new word", word_list)
-
-            count_list = []
-
+    if ord_letter in bufer:
+        print("уже была эта буква, заново")
+        #count_letter = 0
     else:
-        a -= 1
-        print("-" * 10)
-        print("Такой буквы здесь нет\nосталось попыток", a)
-        print("word_random", word_random)
-        print("Word_list", word_list)
-    print(count_list)
+        if letter in word_random:   # если нет в списке - добавляем
+            bufer.append(ord_letter)
+            print("new bufer",bufer)
+
+        count_list = []
+        b = 0
+        count_letter = word_random.count(letter)
+
+        if count_letter >= 1: # ищем индексы и добавляем в список
+            print("Есть такая буква")
+            for i in word_random:
+                if i == letter:
+                    count_list.append(word_random.index(letter, b))
+                    b = count_list[-1] + 1
+                    sd=4
+            print("индексы", count_list, type(count_list))
+
+            if ord_letter in word_ord: # есть есть индексы - меняем на буквы
+                for f in count_list:
+                    print(word_kod, f)
+                    word_list[f] = word_random[f]
+
+            print("new word", word_list, " count_list", count_list) # потом поеменять - вывод как положено
+
+            #count_list = []
+            #b = 0
+        else:
+            a -= 1
+            print("-" * 30)
+            print("Такой буквы здесь нет\nосталось попыток", a)
+            print("word_random", word_random)
+            print("Word_list", word_list)
+            print("ord", word_ord)
+        print(count_list)
 else:
     print("Игра окончена")
 
