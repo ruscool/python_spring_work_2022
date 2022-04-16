a61691a1f4ca2988c4ab11bb2ddf1883a5594802
ruslan <ruscool79@yandex.ru>
2022-04-16 17:23:54 +0300

    подчищено task13, сделан задание task14.py

--- python_spring_work_2022/lesson__4__13.04.2022/hometask/task14.py
+++ python_spring_work_2022/lesson__4__13.04.2022/hometask/task14.py
@@ -10,11 +10,11 @@ mass = [1,2,17,54,30,89,2,1,6,2]
 Для числа 1 индексы двух ближайших чисел: 0 и 7
 Для числа 2 индексы двух ближайших чисел: 6 и 9
 """
-
-mass = [1, 2, 17, 16, 2, 30, 51, 1, 2, 70, 3, 2, 1]
+mass = [1, 2, 17, 54, 30, 89, 2, 1, 6, 2]
+mass2 = [1, 2, 17, 16, 2, 30, 51, 1, 2, 70, 3, 5, 2, 4, 2, 1]
 a = 0
 print(mass)
-zapros = int(input("Число: "))
+zapros = int(input("Число из массива: "))
 
 summaIndex = mass.count(zapros)
 
@@ -24,9 +24,17 @@ if summaIndex > 1:
         if i == zapros:
             b.append(mass.index(zapros, a))
             a = b[-1] + 1
-
     print("Число", zapros, "есть по Индексам", b)
+    x, y = 0, 0
+    min_dlina = len(mass)
+    for f in range(len(b) - 1):
+        ac = int(b[f + 1] - b[f])
+        if min_dlina > ac:
+            min_dlina = ac
+            x, y = b[f], b[f + 1]
+    print("Для числа", zapros, "индексы двух ближайших чисел:", x, "и", y)
 elif summaIndex <= 0:
     print("нет числа в массиве")
 else:
-    print("Число", zapros, "Индекс:", mass.index(zapros))
+    print('Индекс всего один')
+
