129ac3bac77c55d0b357a65802e1fe6b32bca591
ruslan <ruscool79@yandex.ru>
2022-04-21 13:25:57 +0300

    add task19.py

---
+++ python_spring_work_2022/unit_one/task19.py
@@ -0,0 +1,33 @@
+# todo: Требуется создать csv-файл «algoritm.csv» со следующими столбцами:
+"""– id - номер по порядку (от 1 до 10);
+– текст из списка algoritm
+
+algoritm = [ "C4.5" , "k - means" , "Метод опорных векторов" , "Apriori" ,
+"EM" , "PageRank" , "AdaBoost", "kNN" , "Наивный байесовский классификатор" , "CART" ]
+
+Каждое значение из списка должно находится на отдельной строке.
+"""
+
+algoritm = ["C4.5", "k - means", "Метод опорных векторов", "Apriori",
+            "EM", "PageRank", "AdaBoost", "kNN", "Наивный байесовский классификатор", "CART"]
+rows = ["id", "text"]
+
+
+# сделано как говорится из того, что знаем на данный момент
+# в будущем можно переделать через import csv
+
+def create_csv(info, rows):
+    f = open("algoritm.csv", "w+", encoding="utf-8")
+    for i in rows:
+        if i == rows[-1]:
+            f.write('\"' + i + '\"' + '\n')
+        else:
+            f.write('\"' + i + '\"' + ',')
+    for new_line in info:
+        f.write(str(info.index(new_line)) + ',\"' + new_line + '\"\n')
+    f.seek(0)
+    print(f.read())
+    f.close()
+
+
+create_csv(algoritm, rows)
