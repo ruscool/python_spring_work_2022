6085fc2656bb067a41d91a3990ff1e4b804a28ef
ruslan <ruscool79@yandex.ru>
2022-04-23 00:07:56 +0300

    второе задание

---
+++ python_spring_work_2022/Lessons and classwork/lection__8__22.04.2022/classwork/task2.py
@@ -0,0 +1,60 @@
+# todo: Шифр Цезаря
+"""Описание шифра.
+В криптографии шифр Цезаря, также известный шифр сдвига, код Цезаря или сдвиг Цезаря,
+является одним из самых простых и широко известных методов шифрования.
+Это тип подстановочного шифра, в котором каждая буква в открытом тексте заменяется буквой на некоторое
+фиксированное количество позиций вниз по алфавиту. Например, со сдвигом влево 3, D будет заменен на A,
+E станет Б, и так далее. Метод назван в честь Юлия Цезаря, который использовал его в своей частной переписке.
+
+Задача.
+Считайте файл message.txt и зашифруйте  текст шифром Цезаря, при этом символы первой строки файла должны
+циклически сдвигаться влево на 1, второй строки — на 2, третьей строки — на три и т.д.
+В этой задаче удобно считывать файл построчно, шифруя каждую строку в отдельности.
+В каждой строчке содержатся различные символы. Шифровать нужно только буквы латинского алфавита.
+"""
+
+
+def count_line(file):
+    "Подсчет строк в файле"
+    f = open(file, "r")
+    text = f.read()
+    count_line = text.count("\n")
+    return count_line
+
+
+def open_file(alfa, file, lines):
+    "Шифрование"
+    shag = 1
+    digi = "0123456789"
+    english = ''.join([chr(i) for i in range(ord("a"), ord("a") + 26)])
+    hz = ''.join([chr(i) for i in range(35, 48)])
+    f = open(file, 'r')
+    for i in range(lines):
+        file = f.readline()
+        new_line = file.lower().strip()
+        for letter in new_line:
+            if letter in alfa: # шифрование русских букв
+                new_line = new_line.replace(letter, alfa[alfa.index(letter) - shag], 1)
+        for letter in new_line:
+            if letter in digi: # цифры
+                new_line = new_line.replace(letter, digi[digi.index(letter) - shag], 1)
+        for letter in new_line:
+            if letter in english: # английский алфавит
+                new_line = new_line.replace(letter, english[english.index(letter) - shag], 1)
+        shag += 1
+        print(new_line)
+    f.close()
+
+
+def cezar_shifr():
+    "Буквы алфавита"
+    alfavit = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+    a_lower = alfavit.lower()
+    return a_lower
+
+
+file = "message.txt"
+shifr = cezar_shifr()
+count_lines = count_line(file)
+open_file(shifr, file, count_lines)
+
