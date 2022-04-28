f2304bb22f4554e51a7d2bdecaa1bc85bafc08b2
ruslan <ruscool79@yandex.ru>
2022-04-28 23:05:18 +0300

    начинает работать

--- python_spring_work_2022/unit_one/task27.py
+++ python_spring_work_2022/unit_one/task27.py
@@ -33,17 +33,18 @@ def begin():
     a = 10
     b = random.randint(0, 100)
     d = 0
-    # file = "game_dump.pkl"
+    c = 0
     if not menu.isdigit():
         return begin()
     else:
         menu = int(menu)
     while True:
         if menu == 1:
-            return game(a, b, d)
+            return game(a, b, d, c)
         elif menu == 2:
-            load_game(file)
-            game(a, b, d)
+            ll = load_game(file)  # список файлов для выбора
+            # print("ll", ll)
+            return game(ll[0], ll[1], ll[2], c)
         elif menu == 3:
             break
         else:
@@ -52,10 +53,11 @@ def begin():
 
 
 def end_game():
-    return print("Спасибо")
+    print("Спасибо")
+    return -1
 
 
-def save_game(a, b, d):
+def save_game(a, b, d, c):
     global FILE
     file = FILE
     print("\nСохранить игру?", "\n1. - Продолжить", "\n2. - Сохранить игру и выйти")
@@ -64,72 +66,104 @@ def save_game(a, b, d):
         save = int(save)
     else:
         print("Выберите номер из списка")
-        return save_game(a, b, d)
+        return save_game(a, b, d, c)
     match save:
         case 1:
-            return game(a, b, d)
+            return game(a, b, d, c)
         case 2:
-            print(2)  # сохранить два файла - один пикл, второй логгер
-
-            obj = from_pickle(file)
+            new_dict = from_pickle(file)
             dt = datetime.datetime.now()
             log = dt.strftime('%d%m%Y_%H%M%S')
-            new_dict = obj + [{log: [a, b, d]}]
-            print({log: [a, b, d]})
+            new_dict[log] = [a, b, d]  # присваивание
+            # print({log: [a, b, d]})
             to_pickle(new_dict, file)
             print("Игра сохранена под номером: ", log)
-            return end_game()
+            end_game()
+            return -1
         case _:
             print("Номер? ")
-            return save_game(a, b, d)
+            return -1
 
 
 def last_five():
-    pass
+    f = input("Выберите номер записи (от 1 до 5) ")
+    if f.isdigit():
+        f = int(f)
+        if f > 5 or f < 5:
+            f = 5
+    else:
+        print("Не верный выбор, загружаем последнюю сохранненную игру")
+        f = 5
+    return f
 
 
 def load_game(file):
-    obj = from_pickle(file)  # остановился - надо сделать выбор из посл 5 файлов - данные из словаря
-    number = "28042022_211107"
-    print(obj)
-    for i in obj:
-        if i == number:
-            print(True)
-            # a, b, d = number[0], number[1], number[2]
-            # return game(a, b, d, file)
+    obj = from_pickle(file)
+    count = len(obj)
+    if count > 5:
+        list_keys = list(obj.keys())
+        list_keys.sort()
+        new_list = list_keys[-5:]
+        nn = {}
+        for i in new_list:
+            nn[i] = obj[i]
+    obj = nn
+    # последние 5 записей
+    for g in range(5):
+        print(g + 1, new_list[g])
+    # выбор файла
+    f = last_five()
+    n_list = new_list[f - 1]
+    # возврат списка значений останова игры
+    for i, v in obj.items():
+        if i == n_list:
+            return v
 
 
-def game(a, b, dobro):
-    global FILE
-    file = FILE
-    # print("b=", b)  # del
+def game(a, b, dobro, c):
     if dobro == 0:
         print("Добро пожаловать")
         dobro = 1
     else:
         print("Число попыток осталось", a)
+
     d = dobro
     while a > 0:
         c = input("Угадай число от 0 до 100: ")
-        if c == "s" or c == "S":
-            c = int(0)
-            return save_game(a, b, d)
-        else:
-            if c.isdigit():
-                c = int(c)
-            else:
-                print('Необходимо вводить цифры, для сохранения нажать "s""')
-                game(a, b, d)
         a -= 1
-        if c == b:
-            print("Угадали, конец игры")
-            return end_game()
-        elif c < b:
-            print("Нет, больше - еще", a, "попыток")
-        elif c > b:
-            print("Нет, меньше - еще", a, "попыток")
+        if a == 0:
+            print("Не угадали, попыток больше нет")
+            # end_game()
+            # break
+            return -1
+        if c == "s" or c == "S":
+            # a += 1
+            c = 0
+            return save_game(a, b, d, c)
+        elif c.isdigit():
+            c = int(c)
+            if c == b:
+                # a = 0
+                # print(a)
+                print("Угадали, конец игры")
+                end_game()
+                break
+                # return end_game()
+            elif c < b:
+
+                print("Нет, больше - еще", a, "попыток")
+            elif c > b:
+                print("Нет, меньше - еще", a, "попыток")
+        else:
+            print('Необходимо вводить цифры, для сохранения нажать "s""')
+            # game(a, b, d)
     else:
         print("Не угадали, попыток больше нет")
+        end_game()
+    return -1
 
 
-begin()
+r = 0
+if r == 0:
+    begin()
+    r = 1
