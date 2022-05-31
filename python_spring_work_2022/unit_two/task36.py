10a20b99ca151c8299660b558aec176227c7e75b
ruslan <ruscool79@yandex.ru>
2022-05-31 21:33:19 +0300

    task36.py

---
+++ python_spring_work_2022/unit_two/task36.py
@@ -0,0 +1,94 @@
+# todo: Сделать рефакторинг кода задачи 35.
+#  1. Реализовать из класса DB синглтон. Экземляр класса должен быть единственным.
+#  2. Сделать класс View абстрактным, а также метод render() абстрактным
+#  3. Реализовать  фабрику FabConsoleView в которой пораждаются экзепляры
+#     классов TestView, QuestionView и AuthView
+
+# 1.
+# class DB:
+#     __instance__ = None
+#
+#     def __init__(self):
+#         # Проверяем конструктор на сущ. экземпляр
+#         if DB.__instance__ is None:
+#             DB.__instance__ = self
+#         else:
+#             raise Exception("We can not creat another class")
+#
+#     @staticmethod
+#     def get_instance():
+#         # We define the static method to fetch instance
+#         if not DB.__instance__:
+#             DB()
+#         return DB.__instance__
+
+
+# 2.
+# from abc import ABC, abstractmethod
+# class View(ABC):
+#     """ 'Абстрактный' класс для потомков """
+#     @abstractmethod
+#     def render(self):
+#         print('Virtual')
+#
+#
+# class RegistrationView(View):
+#     """В классе перегружаем виртуальный метод  render от родителя"""
+#
+#     def render(self, data):
+#         """Метод реализует отрисовку регистрации пользователя"""
+#
+#
+# class LoginView(View):
+#     """В классе перегружаем виртуальный метод  render от родителя"""
+#
+#     def render(self, data):
+#         """Метод реализует отрисовку входа по логину и паролю для зарегистрированного пользователя"""
+
+
+# 3.
+
+class View():
+    """ 'Абстрактный' класс для потомков """
+
+    def render(self):
+        pass
+
+#TestView, QuestionView и AuthView
+class QuestionView(View):
+    """В классе перегружаем виртуальный метод  render от родителя"""
+
+    def render(self):
+        """Метод реализует отрисовку вопроса с вариантами ответа и строкой выбора варианта"""
+        print('class', type.__class__)
+
+
+class TestView(View):
+    """В классе перегружаем виртуальный метод  render от родителя"""
+
+    def render(self):
+        """Метод реализует отрисовку регистрации пользователя"""
+        print('class', type.__class__)
+
+class AuthView(View):
+    """В классе перегружаем виртуальный метод  render от родителя"""
+
+    def render(self):
+        """Метод реализует отрисовку регистрации пользователя"""
+        print('class', type.__mro__)
+
+class ViewView:
+    def getView(self, metod_view):
+        if metod_view == 'QuestionView':
+            return QuestionView()
+        elif metod_view == 'RegistrationView':
+            return TestView()
+        elif metod_view == 'AuthView':
+            return AuthView()
+        else:
+            pass
+
+
+obj = ViewView()
+method = obj.getView("AuthView")
+method.render()
