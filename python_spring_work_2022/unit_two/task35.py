81c0d61756bbfcfb1e9406dcc031188441677106
ruslan <ruscool79@yandex.ru>
2022-06-01 00:06:10 +0300

    Рабочий вариант ver 1_0

--- python_spring_work_2022/unit_two/task35.py
+++ python_spring_work_2022/unit_two/task35.py
@@ -7,6 +7,7 @@ import psycopg2 as ps
 from datetime import datetime as dtime
 import datetime
 from abc import ABC, abstractmethod
+from art import *
 
 
 class Db:
@@ -30,20 +31,26 @@ class Profile(Db):
      пользователя соответсвенно'''
 
     # В констукторе инициализируем атрибуты сущности Profile
-    def __init__(self, login, u_password, f_name, ouch, l_name, age, tel, email, name, user, password):
+    def __init__(self, login, u_password, name='testsystem', user='testsystem', password='1234test'):
         super().__init__(name, user, password)
+        self.email = None
+        self.tel = None
+        self.l_name = None
+        self.ouch = None
+        self.f_name = None
+        self.age = None
         self.login = login
         self.user_password = u_password
+        self.get_connection()
+
+    # в аргументе conn передается дискриптор подключения к БД
+    def set_profile(self, f_name, ouch, l_name, age, tel, email):
         self.f_name = f_name
         self.ouch = ouch
         self.l_name = l_name
         self.age = age
         self.tel = tel
         self.email = email
-        self.get_connection()
-
-    # в аргументе conn передается дискриптор подключения к БД
-    def set_profile(self):
         with self.conn.cursor() as cur:
             print('Регистрация нового пользователя')
             try:
@@ -83,13 +90,13 @@ class Profile(Db):
                            where p.login ='{self.login}'""")
             res_login = cur.fetchone()
             if res_login is None:
-                print('Нет такого пользователя, хотите зарегистрироваться?')
-                return 0
+                data = str('Нет такого пользователя, хотите зарегистрироваться?')
+                return RegistrationView.render(RegistrationView, data, self.login, self.password)
             else:
                 valid = bcrypt.checkpw(self.password.encode(), res_login[1].encode())
                 if valid:
-                    print(f'Вы вошли под пользователем {self.login}')
-                    return 1
+                    data = str(f'Вы вошли под пользователем {self.login}')
+                    return LoginView.render(LoginView, data)
                 else:
                     print('Неверный пароль, попробуйте заново')
                     return 0
@@ -98,9 +105,13 @@ class Profile(Db):
 class Auth:
     def __init__(self):
         self.if_true = None
-        self.is_auth = False
 
-    def login(self, user: int) -> bool:
+    # self.conn = Db('testsystem', 'testsystem', '1234test').get_connection()
+    # self.is_auth = False
+    # self.login = login
+    # self.password = password
+
+    def login(self, user) -> bool:
         """
         Возврат true - если пользователь вошел в систему
         :param user:
@@ -123,15 +134,16 @@ class Auth:
 
 
 class Testsystem:
-    def __init__(self):
+    def __init__(self, data, s_quest):
+        self.data = data
         self.dt = datetime.datetime.now()
-        self.questions_in_test = None
+        self.questions_in_test = s_quest
         self.num_list = self.show_list()
         self.conn = Db('testsystem', 'testsystem', '1234test').get_connection()
 
     def show_list(self):
-        data = Test()
-        self.data = data.get_list()
+        # data = Test()
+        # self.data = data.get_list()
         print('Выберите номер теста')  # in View
         l = []
         for key in self.data:
@@ -150,9 +162,6 @@ class Testsystem:
     def show_questions(self):
         print('\n' * 30)
         print(f'Выбран тест под номером {self.test_number} и вопросы к нему')
-        get = Test()
-        self.questions_in_test = get.get_questions()
-
         for i in self.questions_in_test:
             print(i[0], i[1])
         print('\nНачнем тест - дается время 10 мин')
@@ -160,7 +169,6 @@ class Testsystem:
             print(i, end=' ')
             sleep(0.1)
         self.dt = dtime.now().timestamp()
-
         return self.questions_in_test
 
     def my_metods(self, answers):
@@ -172,15 +180,14 @@ class Testsystem:
         :param answers:
         :return: (dt,self.test_number, answers)
         """
-        # self.dt = dt
         self.for_result = answers
         self.answers = str(answers)
 
-        # здесь сделать слияние для БД и расчета результата теста
+        # здесь сделать список для БД и расчета результата теста
         # ввод (дата, номер теста, ответы) - в БД - для хранения
         # разбор ответов - пока только в Да или нет и в %
-        print(self.dt, self.test_number, self.answers)
-        print('я здесь')
+        # print(self.dt, self.test_number, self.answers)
+        # print('я здесь')
         return self.dt, self.test_number, self.answers
 
     def save_in_base(self):
@@ -195,8 +202,8 @@ class Testsystem:
         nn = time.strftime("%M:%S", time.localtime(how_long))
         print(' ' * 30)
         print(f'Тест пройдет за {nn}')
-        print(self.for_result)
-        if sum(self.for_result[2:-1:3]) / 10 * 100 > 75:
+        # print(self.for_result)
+        if sum(self.for_result[2:-1:3]) / (len(self.for_result) - 1) * 100 > 75:
             test = 'Сдан'
         else:
             test = 'Не сдан'
@@ -205,22 +212,26 @@ class Testsystem:
 
 
 class Test:
-    def __init__(self):
+    def __init__(self, data):
         self.conn = Db('testsystem', 'testsystem', '1234test').get_connection()
+        self.count_aquestions = 10
+        self.login = login
+        self.password = password
+        self.if_true = data
+        self.run()
 
     def run(self):
-        new_user = Profile('usr89', '1234test', 'Иван', 'Петрович', 'Сидоров', 29, 89234238937, 'ru@ru.com',
-                           'testsystem', 'testsystem', '1234test')
-
-        self.if_user = new_user.get_profile()
-        self.t = Testsystem()
-        self.v = QuestionView()
-        data = self.t.show_questions()
-        self.get_questions()
-        self.answers = self.v.render(data)
-        self.t.my_metods(self.answers)
-        # self.t.save_in_base()
-        self.t.result_test()
+        if self.if_true == 1:
+            self.five_list = self.get_list()
+            self.count_questions = self.get_questions()
+            self.t = Testsystem(self.five_list, self.count_questions)
+            self.v = QuestionView()
+            data = self.t.show_questions()
+            self.get_questions()
+            self.answers = self.v.render(data)
+            self.t.my_metods(self.answers)
+            # self.t.save_in_base()
+            self.t.result_test()
 
     def get_list(self) -> list:
         """
@@ -240,7 +251,7 @@ class Test:
         with self.conn.cursor() as cur:
             cur.execute(f"""select * from question q """)
             res_login = cur.fetchall()
-            random_questions = random.sample(res_login, 10)
+            random_questions = random.sample(res_login, self.count_aquestions)
             random_questions.sort()
             # print(random_questions)
         return random_questions
@@ -267,9 +278,8 @@ class QuestionView(View):
     def render(self, data):
         print(' ' * 30)
         in_bd = []
-        print(data)
         for n in data:
-            print('Вопрос', n[1])
+            print('Вопрос:', n[1])
             print('Выберите ответ:')
             with self.conn.cursor() as cur:
                 print()
@@ -301,24 +311,46 @@ class QuestionView(View):
                     print('Выберите из списка по номеру')
         self.end_dt = dtime.now().timestamp()
         in_bd.append(self.end_dt)
-        print('Тест окончен\n')
+        print('Тест окончен')
         return in_bd
 
 
 class RegistrationView(View):
     """В классе перегружаем виртуальный метод  render от родителя"""
 
-    def render(self, data):
+    def render(self, data, login, password):
         """Метод реализует отрисовку регистрации пользователя"""
+        print(data)
+        answer = int(input('1.Да \n2.Нет\nНомер: '))
+        if answer == 1:
+            new_user = Profile(login, password)
+            print('-' * 50)
+            print('Регистрация нового пользователя')
+            f_name = input('Введите Имя ')
+            ouch = input('Введите Отчество ')
+            l_name = input('Введите Фамилию ')
+            age = input('Введите возраст ')
+            tel = input('Введите телефон ')
+            e_mail = input('Введите почту ')
+            new_user.set_profile(f_name, ouch, l_name, age, tel, e_mail)
+
 
 
 class LoginView(View):
+    # new_user = Profile('usr89', '1234test', 'Иван', 'Петрович', 'Сидоров', 29, 89234238937, 'ru@ru.com',
+    #                    'testsystem', 'testsystem', '1234test')
     """В классе перегружаем виртуальный метод  render от родителя"""
 
     def render(self, data):
+        self.data = data
         """Метод реализует отрисовку входа по логину и паролю для зарегистрированного пользователя"""
+        print(self.data)
+        return Test(1)
 
 
 if __name__ == "__main__":
-    main = Test()
-    main.run()
+    tprint("Tests", font="rand-medium")
+    login = input('Введите логин: ')
+    password = input('Введите пароль: ')
+    main = Profile(login, password)
+    main.get_profile()
